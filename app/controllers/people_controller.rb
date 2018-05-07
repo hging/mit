dd7262deb78e51b98b144c925761b03e83c73234
hging <hging3@gmail.com>
2018-05-07 22:02:35 +0800

    fix page

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,5 +1,19 @@
 class PeopleController < ApplicationController
-  def index
+  def engineers
+    per = params[:per] || 18
+    page = params[:page] || 1
+    @all_first_words = Person.pluck(:first_word).uniq.sort
+    @people = Person.ransack({ first_word_eq: params[:q] }).result.page(page).per(per)
+  end
+
+  def faculty_pls
+    per = params[:per] || 18
+    page = params[:page] || 1
+    @all_first_words = Person.pluck(:first_word).uniq.sort
+    @people = Person.ransack({ first_word_eq: params[:q] }).result.page(page).per(per)
+  end
+
+  def students
     per = params[:per] || 18
     page = params[:page] || 1
     @all_first_words = Person.pluck(:first_word).uniq.sort
--- config/routes.rb
+++ config/routes.rb
@@ -13,14 +13,29 @@ Rails.application.routes.draw do
 
   resources :research, :only => %w(index) do
     collection do
-      get 'communications'
-      get 'control'
-      get 'inference'
+      get 'fog'
+      get 'cloud'
+      get 'distributed'
+      get 'communication'
+      get 'network_design'
+      get 'big_data'
+      get 'network_information'
+      get 'network_optimzation'
     end
   end
 
   resources :activities, :only => %w(index show)
 
-  get :people, :to => 'people#index'
+  # get :people, :to => 'people#index'
+  namespace :people do
+    get :engineers
+    get :faculty_pls
+    get :students
+  end
+
+  namespace :labs do
+    get :dial
+    get :data
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
