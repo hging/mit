01311b1a1ba71578729290f35c240574c6ee185d
hging <hging3@gmail.com>
2018-07-25 00:26:57 +0800

    fix

--- app/controllers/research_controller.rb
+++ app/controllers/research_controller.rb
@@ -4,9 +4,9 @@ class ResearchController < ApplicationController
     page = params[:page] || 1
     @result = Activity.highlights.active.ransack({:content_matches => "%#{params[:q]}%"}).result.where(:published => true).page(page).per(per)
   end
-  def activity_result
+  def archive_result
     per = params[:per] || 10
     page = params[:page] || 1
-    @result = Activity.activity.active.ransack({:content_matches => "%#{params[:q]}%"}).result.where(:published => true).page(page).per(per)
+    @result = Activity.archive.active.ransack({:content_matches => "%#{params[:q]}%"}).result.where(:published => true).page(page).per(per)
   end
 end
