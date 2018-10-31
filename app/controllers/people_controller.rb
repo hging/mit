a2f14a2a79c03154a4d5f9472696318603f7dfa3
hging <hging3@gmail.com>
2018-10-31 10:09:48 +0800

    fix

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -7,7 +7,13 @@ class PeopleController < ApplicationController
     @all_first_words = Person.pluck(:first_word).uniq.sort
     @people = Person.ransack({ first_word_eq: params[:q] }).result.page(page).per(per)
   end
+
+  def group;end
+  def publication;end
+  def teaching;end
+
   private
+
   def set_person
     @person = Person.find(params[:id])
   end
