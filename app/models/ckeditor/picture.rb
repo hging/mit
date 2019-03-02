a2b8e97174f38d73051d46135df72a5d2d86b0c5
hging <hging3@gmail.com>
2019-03-02 22:18:37 +0800

    fix bug

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -17,6 +17,16 @@ class Ckeditor::Picture < Ckeditor::Asset
   mount_uploader :data, CkeditorPictureUploader, mount_on: :data_file_name
 
   def url_content
-    url(:content)
+    url
+  end
+
+  def url(version=nil)
+    if version == :thumb
+      url
+    elsif version == :content
+      url
+    else
+      super
+    end
   end
 end
